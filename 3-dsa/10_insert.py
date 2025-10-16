387c5f9256d5b2f4021be471c24558837f18418a
Ubaydullo Asatullaev <Ubaydullohazard@gmail.com>
2025-10-17 00:33:24 +0900

    Create 10_insert.py

---
+++ 3-dsa/10_insert.py
@@ -0,0 +1,65 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def get(self, index):
+        if index < 0 or index > self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp 
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+
+    def print_list(self):
+        temp = self.head
+        while (temp):
+            print(temp.value)
+            temp = temp.next
+    
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return None
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index - 1)  
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+
+linkedlist = LinkedList(0)
+linkedlist.append(2)
+linkedlist.insert(1,1)
+linkedlist.print_list()
